cb02c276b215f5b9a9508d6bdbad4c081a14d46d
Darya <dasha.putyato@gmail.com>
2024-08-03 14:15:21 +0300

    new_lambda

---
+++ lambda_func.py
@@ -0,0 +1,17 @@
+# def double(x):
+#     return x*2
+
+
+#print (double(5))
+
+double=lambda x: x*2
+print(double(5))
+
+multiply=lambda x,y:x*y
+print(multiply(5,8))
+
+full_name= lambda first_name,last_name: first_name+" "+last_name
+print(full_name('d','p'))
+
+age=lambda age:True if age>=18 else False
+
