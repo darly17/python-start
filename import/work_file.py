1716607c48435b0e30fcd820b7c815a5cb2da064
darly17 <144932731+darly17@users.noreply.github.com>
2024-12-08 15:22:54 +0300

    Update work_file.py

--- import/work_file.py
+++ import/work_file.py
@@ -1,14 +1,10 @@
 import newmod
 x=8
-#просмотр значений для работы
+
 print(dir(newmod))
-#расшифровка конкретного функционала модуля
 print(help(newmod.newf))
-#вызов функции из модуля
 print(newmod.newf(3))
 
-
- #импорт всго функционала модуля или отдельных значений
 from newmod import *
 
 
